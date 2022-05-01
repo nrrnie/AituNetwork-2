cb76a0acf23182a1be2e04fe209ca7df03c1cafb
mercurial <dias.nespayev@gmail.com>
2022-05-01 19:07:55 +0600

    feat: add profile page

--- aituNetwork/users/routes.py
+++ aituNetwork/users/routes.py
@@ -5,4 +5,4 @@ from aituNetwork.users import users
 
 @users.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', current_user={}, profile_user={})
