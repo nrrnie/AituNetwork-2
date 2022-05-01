9b637f15c6d13b75f5f61399f4fa830926b44fac
mercurial <dias.nespayev@gmail.com>
2022-05-01 15:56:38 +0600

    feat: add register page

--- aituNetwork/auth/routes.py
+++ aituNetwork/auth/routes.py
@@ -5,3 +5,8 @@ from aituNetwork.auth import auth
 @auth.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+
+@auth.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
