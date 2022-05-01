15287012ddbf0ea20a65c27aef6f922ddc2d28b0
mercurial <dias.nespayev@gmail.com>
2022-05-01 16:01:45 +0600

    feat: add config

---
+++ config.py
@@ -0,0 +1,15 @@
+from os import environ
+
+from urllib.parse import quote_plus
+
+db = {
+    'user': environ.get('DATABASE_USER'),
+    'password': environ.get('DATABASE_PASSWORD'),
+    'host': environ.get('DATABASE_HOST'),
+    'name': environ.get('DATABASE_NAME')
+}
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db["user"]}:{quote_plus(db["password"])}@{db["host"]}/{db["name"]}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
