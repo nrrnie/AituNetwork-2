ea0cba497bd4ac90de406854b7cbb9fbd7142388
mercurial <dias.nespayev@gmail.com>
2022-05-01 19:47:22 +0600

    feat: add `slug` to `users.profile`

--- aituNetwork/users/routes.py
+++ aituNetwork/users/routes.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, session
 
 from aituNetwork.users import users
+from aituNetwork.models import Users
 
 
-@users.route('/profile')
-def profile():
-    return render_template('profile.html', current_user={}, profile_user={})
+@users.route('/<slug>')
+def profile(slug: str):
+    user = Users.query.filter_by(slug=slug).first()
+
+    if user is None:
+        return 'user is not found'
+
+    current_user = session['user']
+
+    return render_template('profile.html', current_user=current_user, profile_user=user)
