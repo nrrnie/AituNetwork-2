3f20ebd281bb4f7efc36e8f741b5fca824f4310a
mercurial <dias.nespayev@gmail.com>
2022-05-01 18:10:37 +0600

    feat: log user in

--- aituNetwork/auth/routes.py
+++ aituNetwork/auth/routes.py
@@ -1,9 +1,25 @@
-from flask import render_template
+from flask import render_template, request, flash
+from passlib.hash import sha256_crypt
 from aituNetwork.auth import auth
 
+from aituNetwork.models import Users
 
-@auth.route('/login', methods=['GET'])
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    barcode = request.form.get('barcode')
+    password = request.form.get('password')
+
+    user = Users.query.filter_by(barcode=barcode).first()
+
+    if user is not None and sha256_crypt.verify(password, user.password):
+        return 'main page'
+
+    flash('Barcode or password is wrong')
+
     return render_template('login.html')
 
 
