2389c8eb3ed8508f5ac3916a3e12dbe361d7a52a
mercurial <dias.nespayev@gmail.com>
2022-05-01 16:12:24 +0600

    feat: add database

--- aituNetwork/__init__.py
+++ aituNetwork/__init__.py
@@ -1,10 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+
+from config import Config
 
 from aituNetwork.auth import auth
 
 
+db = SQLAlchemy()
+
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(auth, url_prefix='/auth')
 
