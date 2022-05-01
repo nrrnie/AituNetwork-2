822243ea73e23c17aaa96885f11fcb7666496e46
mercurial <dias.nespayev@gmail.com>
2022-05-01 18:24:05 +0600

    feat: register users

--- aituNetwork/auth/routes.py
+++ aituNetwork/auth/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, request, flash
+from flask import redirect, url_for
 from passlib.hash import sha256_crypt
-from aituNetwork.auth import auth
 
+
+from aituNetwork.auth import auth
 from aituNetwork.models import Users
+from aituNetwork import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -23,6 +26,30 @@ def login():
     return render_template('login.html')
 
 
-@auth.route('/register', methods=['GET'])
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    barcode = request.form.get('barcode')
+    first_name = request.form.get('first-name')
+    last_name = request.form.get('last-name')
+    password = request.form.get('password')
+    password_confirm = request.form.get('password-confirm')
+
+    if password == password_confirm:
+        user = Users.query.filter_by(barcode=barcode).first()
+        if user is None:
+            hashed_password = sha256_crypt.hash(password)
+            user = Users(barcode=barcode, first_name=first_name, last_name=last_name, password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+
+            flash('User was successfully created!')
+            return redirect(url_for('auth.login'))
+        else:
+            flash('Barcode is already registered')
+    else:
+        flash('Passwords does not match')
+
     return render_template('register.html')
