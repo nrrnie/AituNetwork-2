9f762c192cbdf26806c2e34ea9f23d9dee994138
mercurial <dias.nespayev@gmail.com>
2022-05-01 15:47:25 +0600

    feat: add login page

--- aituNetwork/auth/routes.py
+++ aituNetwork/auth/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from aituNetwork.auth import auth
 
 
 @auth.route('/login', methods=['GET'])
 def login():
-    return 'login'
+    return render_template('login.html')
