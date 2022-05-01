cde7bb2f5c7eed85f1eb6a4b952470b809f3fee7
mercurial <dias.nespayev@gmail.com>
2022-05-01 20:01:06 +0600

    feat: add `auth_required` decorator

--- aituNetwork/users/routes.py
+++ aituNetwork/users/routes.py
@@ -1,10 +1,21 @@
 from flask import render_template, session
-
+from flask import redirect, url_for
+import functools
 from aituNetwork.users import users
 from aituNetwork.models import Users
 
 
-@users.route('/<slug>')
+def auth_required(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if session.get('user') is None:
+            return redirect(url_for('auth.login'))
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@users.route('/<slug>', methods=['GET'])
+@auth_required
 def profile(slug: str):
     user = Users.query.filter_by(slug=slug).first()
 
