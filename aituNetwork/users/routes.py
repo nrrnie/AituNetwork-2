7a61055f92d6854005dfa8a938491305b3099b08
mercurial <dias.nespayev@gmail.com>
2022-05-03 22:39:13 +0600

    feat: save picture in filesystem

--- aituNetwork/users/routes.py
+++ aituNetwork/users/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, session
-from flask import redirect, url_for
+from flask import request, render_template, session
+from flask import redirect, url_for, flash
 import functools
 from aituNetwork.users import users
 from aituNetwork.models import Users
+from utils import picturesDB
 
 
 def auth_required(func):
@@ -27,8 +28,17 @@ def profile(slug: str):
     return render_template('profile.html', current_user=current_user, profile_user=user)
 
 
-@users.route('/settings', methods=['GET'])
+@users.route('/settings', methods=['GET', 'POST'])
 @auth_required
 def settings():
+    if request.method == 'GET':
+        return render_template('settings.html', user=session['user'])
 
-    return render_template('settings.html', user=session['user'])
+    picture = request.files.get('profile-picture')
+    if picture:
+        picture_name = picturesDB.add_picture('profile-image', picture)
+        # save picture_name to database
+
+    flash('Info was updated')
+    return redirect(url_for('users.settings'))
+
