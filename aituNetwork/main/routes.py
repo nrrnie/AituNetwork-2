ba2718c44e8c83a0e46c892a8d43250fffbf9eb1
mercurial <dias.nespayev@gmail.com>
2022-05-06 15:07:44 +0600

    chore: add home page

--- aituNetwork/main/routes.py
+++ aituNetwork/main/routes.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, session
 from aituNetwork.main import main
+from utils import auth_required
 
 
 @main.route('/home', methods=['GET'])
+@auth_required
 def home():
-    return render_template('home.html')
+    user = session['user']
+    return render_template('home.html', user=user)
