3ee5ac0edceda2629eaf2d071d970f26d8c80ddc
Alpaca <ayazhan1121@gmail.com>
2022-05-07 14:55:40 +0600

    feat: add friend

--- aituNetwork/users/routes.py
+++ aituNetwork/users/routes.py
@@ -1,7 +1,7 @@
 from flask import request, render_template, session
 from flask import redirect, url_for, flash
 from aituNetwork.users import users
-from aituNetwork.models import Users, ProfilePictures
+from aituNetwork.models import Users, ProfilePictures, Friends
 from aituNetwork import db
 from utils import picturesDB, auth_required
 
@@ -20,14 +20,23 @@ def profile(slug: str):
 
     user = session['user']
 
-    # current page
-    profile_status = None
-    if user.id == profile_user.id:
-        profile_status = 'own profile'
-    elif user.id != profile_user.id:
-        profile_status = "user's profile"
+    is_my_friend = Friends.query.filter_by(user_id=user.id, friend_id=profile_user.id).first()
+    am_i_friend = Friends.query.filter_by(user_id=profile_user.id, friend_id=user.id).first()
 
-    return render_template('profile.html', user=user, profile_user=profile_user, profile_status=profile_status)
+    # friend_status
+    # 1: I sent request
+    # 2: Profile user sent request
+    # 3: Friends
+
+    friend_status = None
+    if is_my_friend is not None and am_i_friend is not None:
+        friend_status = 3
+    elif is_my_friend is not None:
+        friend_status = 1
+    elif am_i_friend is not None:
+        friend_status = 2
+
+    return render_template('profile.html', user=user, profile_user=profile_user, friend_status=friend_status)
 
 
 @users.route('/settings', methods=['GET', 'POST'])
@@ -39,10 +48,20 @@ def settings():
     picture = request.files.get('profile-picture')
     if picture:
         picture_name = picturesDB.add_picture('profile-pictures', picture)
-        profile_picture = ProfilePictures(user_id=session['user'].id,  name=picture_name)
+        profile_picture = ProfilePictures(user_id=session['user'].id, name=picture_name)
         db.session.add(profile_picture)
         db.session.commit()
 
     flash('Info was updated', 'success')
     return redirect(url_for('users.settings'))
 
+
+@users.route('/add/friend')
+@auth_required
+def add_friend():
+    user_id = request.values.get('user_id')
+    friend_id = request.values.get('friend_id')
+    friend = Friends(user_id=user_id, friend_id=friend_id)
+    db.session.add(friend)
+    db.session.commit()
+    return "Friend request is sent"
