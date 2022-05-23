c22238b1029c6f75b982e152b556b124d006c9a9
mercurial <dias.nespayev@gmail.com>
2022-05-23 08:58:24 +0600

    feat: show posts

--- aituNetwork/main/routes.py
+++ aituNetwork/main/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, session
+from aituNetwork.models import Posts
 from aituNetwork.main import main
 from utils import auth_required
 
@@ -7,4 +8,7 @@ from utils import auth_required
 @auth_required
 def home():
     user = session['user']
-    return render_template('home.html', user=user)
+
+    posts = Posts.get_feed(user.id)
+
+    return render_template('home.html', user=user, posts=posts)
