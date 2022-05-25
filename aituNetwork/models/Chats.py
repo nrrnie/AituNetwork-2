e34f8cc5004bfc19438073fa6197d0a98179262b
mercurial <dias.nespayev@gmail.com>
2022-05-26 05:32:43 +0600

    chore: add `is_chat_exist`

--- aituNetwork/models/Chats.py
+++ aituNetwork/models/Chats.py
@@ -3,3 +3,7 @@ from aituNetwork.models import db
 
 class Chats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    @staticmethod
+    def is_chat_exist(chat_id: int) -> bool:
+        return Chats.query.get(chat_id) is not None
