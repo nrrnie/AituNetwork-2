59e62309d89273baca676ff90fa0d044221f5873
mercurial <dias.nespayev@gmail.com>
2022-05-26 04:22:38 +0600

    ref: rearrange models

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,8 +2,11 @@ from utils.PicturesDB import PicturesDB
 from flask import session, redirect, url_for
 from email.message import EmailMessage
 from smtplib import SMTP_SSL
+from random import randint
 from os import getenv
 import functools
+
+from aituNetwork.models import Users
 
 picturesDB = PicturesDB()
 
@@ -27,3 +30,14 @@ def send_email(to: str, token_link: str, header: str, msg: str):
     with SMTP_SSL('smtp.gmail.com', int(getenv('SMTP_PORT'))) as smtp:
         smtp.login(getenv('SMTP_SENDER'), getenv('SMTP_PASSWORD'))
         smtp.send_message(message)
+
+
+def random_id():
+    mn = 1000000
+    mx = 9999999
+
+    rand = randint(mn, mx)
+    while Users.query.filter_by(id=rand).first() is not None:
+        rand = randint(mn, mx)
+
+    return rand
