10c0ffae8680a3941ef68687cd93cb572ba39988
mercurial <dias.nespayev@gmail.com>
2022-05-26 05:32:49 +0600

    chore: add `is_user_in_chat`

--- aituNetwork/models/UsersChats.py
+++ aituNetwork/models/UsersChats.py
@@ -1,4 +1,5 @@
 from aituNetwork.models import db
+from aituNetwork.models import Chats
 
 
 class UsersChats(db.Model):
@@ -6,3 +7,13 @@ class UsersChats(db.Model):
     chat_id = db.Column(db.Integer, index=True)
     user_id = db.Column(db.Integer, index=True)
     __table_args__ = (db.UniqueConstraint('chat_id', 'user_id'),)
+
+    @staticmethod
+    def is_user_in_chat(user_id: int, chat_id: int) -> bool:
+        if not Chats.is_chat_exist(chat_id):
+            return False
+
+        if UsersChats.query.filter_by(chat_id=chat_id, user_id=user_id).first() is None:
+            return False
+
+        return True
