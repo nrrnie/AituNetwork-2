a23b69ea440e9e8fbf39067f96c59274345168f3
mercurial <dias.nespayev@gmail.com>
2022-05-29 07:31:38 +0600

    chore: add get method

--- aituNetwork/models/Chats.py
+++ aituNetwork/models/Chats.py
@@ -5,5 +5,9 @@ class Chats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     @staticmethod
+    def get(chat_id: int):
+        return Chats.query.get(chat_id)
+
+    @staticmethod
     def is_chat_exist(chat_id: int) -> bool:
         return Chats.query.get(chat_id) is not None
