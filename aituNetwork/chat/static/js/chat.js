80fc0867151df5bfcf83c8633059275d4ce6c339
mercurial <dias.nespayev@gmail.com>
2022-05-29 06:25:26 +0600

    feat: append message when received

--- aituNetwork/chat/static/js/chat.js
+++ aituNetwork/chat/static/js/chat.js
@@ -1,9 +1,17 @@
+let socket = io.connect('http://127.0.0.1:5000', {query: "user_id=" + current_user});
+
+socket.on('message', data => {
+    add_message(data['user_id'], data['message'])
+});
+
 $('document').ready(function () {
     $('#sendMessage').on('click', function () {
         let message_text_element = $('#messageText');
         let message = message_text_element.val();
         message_text_element.val('');
-        add_message(message);
+        add_message(current_user, message);
+
+        socket.emit('message', {user_id: 8, message_text: message})
     });
 
     $('#messageText').on('input', function () {
@@ -13,20 +21,20 @@ $('document').ready(function () {
         }
     })
 
-    function add_message(message) {
+
+})
+
+function add_message(user_id, message) {
         $.ajax({
             url: '/utils/generate-message',
             method: 'POST',
             data: {
-                user_id: 7,
+                user_id: user_id,
                 message: message
             },
-            success: function(data) {
+            success: function (data) {
                 $('#dialogBox').prepend(data);
             }
         })
 
     }
-})
-
-
